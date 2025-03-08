97c57e2c1bf404190899189068341b8cb256ef4f
SVernetCodes <shellivernet@gmail.com>
2025-03-07 19:38:19 -0500

    Create parametersTest.py

---
+++ parametersTest.py
@@ -0,0 +1,27 @@
+import requests
+
+from requests.auth import HTTPBasicAuth
+
+import os
+
+
+
+# Load credentials from environment variables
+
+WORKFLOW_TOKEN = os.getenv("WORKFLOW_TOKEN")
+
+WORKFLOW_SECRET = os.getenv("WORKFLOW_SECRET")
+
+
+
+response = requests.get(
+
+    "https://sandbox.alloy.co/v1/parameters/",
+
+    auth=HTTPBasicAuth(WORKFLOW_TOKEN, WORKFLOW_SECRET)
+
+)
+
+
+
+print(response.json())  # This might show workflow-related details
